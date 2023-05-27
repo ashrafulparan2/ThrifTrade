e0eada3ba52a87f95855fc9edf59f3349127b305
ashrafulparan2 <ashrafulparan@gmail.com>
2023-05-27 19:02:41 +0600

    list-products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <header>TS AMAZONA</header>
-      <main></main>
+      <header>ThriftTrade</header>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p> ${product.price} </p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved</footer>
     </div>
   )
