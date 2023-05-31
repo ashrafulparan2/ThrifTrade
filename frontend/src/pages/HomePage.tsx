777da0279635301ca655afa5eb76a2e7f8c6a134
ashrafulparan2 <ashrafulparan@gmail.com>
2023-05-31 22:10:07 +0600

    added-new-homepage-component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
-import { Carousel, Container } from 'react-bootstrap'
+import { Button, Carousel, Container } from 'react-bootstrap'
 
-import '@splidejs/react-splide/css'
+import Marquee from 'react-fast-marquee'
+
 import { Link } from 'react-router-dom'
 
 // Import Swiper React components
@@ -76,9 +77,7 @@ export default function HomePage() {
 
           <Carousel.Caption>
             <h3>Buy your gadgets</h3>
-            <p>
-              At low price!
-            </p>
+            <p>At low price!</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
@@ -113,8 +112,125 @@ export default function HomePage() {
           ))}
         </Swiper>
       </Container>
+
+      <Container className="home-wrapper-1 py-5">
+        <div className="row">
+          <div className="col-6">
+            <div className="main-banner position-relative ">
+              <img
+                src="../../public/images/main-banner-1.jpg"
+                className="img-fluid rounded-3"
+                alt="main banner"
+              />
+              <div className="main-banner-content position-absolute">
+                <h4>SUPERCHARGED FOR PROS.</h4>
+                <h5>iPad S13+ Pro.</h5>
+                <p>From $999.00 or $41.62/mo.</p>
+                <Link to={'/product/'}>
+                  <Button className="button-color">BUY NOW</Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+              <div className="small-banner position-relative">
+                <img
+                  src="../../public/images/catbanner-01.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>Best Sake</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative">
+                <img
+                  src="../../public/images/catbanner-02.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative ">
+                <img
+                  src="../../public/images/catbanner-03.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+              <div className="small-banner position-relative ">
+                <img
+                  src="../../public/images/catbanner-04.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>But IPad Air</h5>
+                  <p>
+                    From $999.00 <br /> or $41.62/mo.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+
       <HomepageSell />
-      
+
+      <Container className="marque-wrapper home-wrapper-2 py-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="marquee-inner-wrapper card-wrapper">
+              <Marquee className="d-flex">
+                <div className="mx-4 w-25">
+                  <img src="../../public/images/brand-01.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="../../public/images/brand-02.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="../../public/images/brand-03.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="../../public/images/brand-04.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="../../public/images/brand-05.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="../../public/images/brand-06.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="../../public/images/brand-07.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="../../public/images/brand-08.png" alt="brand" />
+                </div>
+              </Marquee>
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
