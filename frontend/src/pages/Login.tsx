31ebb1c4dd1db034c67ede417c298e7378ef1a88
ashrafulparan2 <ashrafulparan@gmail.com>
2023-06-07 00:38:18 +0600

    againLogin

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,138 +1,85 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import styled from "styled-components";
-import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
-import Button from "../components/LoginFolder/Button.js";
-import Icon from "../components/LoginFolder/Icon.js";
-import Input from "../components/LoginFolder/Input.js";
+import React, { useState } from 'react';
+import './signin.css';
 
-import './signin.css'
-class Login extends PureComponent {
-    render() {
-        const FacebookBackground =
-        "linear-gradient(to right, #0546A0 0%, #0546A0 40%, #663FB6 100%)";
-      const InstagramBackground =
-        "linear-gradient(to right, #A12AC4 0%, #ED586C 40%, #F0A853 100%)";
-      const TwitterBackground =
-        "linear-gradient(to right, #56C1E1 0%, #35A9CE 50%)";
-        return (
-            <body className="bdy">
-                  <MainContainer>
-      <WelcomeText>Welcome</WelcomeText>
-      <InputContainer>
-        <Input type="text" placeholder="Email" />
-        <br></br>
-        <Input type="password" placeholder="Password" />
-      </InputContainer>
-      <ButtonContainer>
-        <Button content="Sign In" />
-      </ButtonContainer>
-      <LoginWith></LoginWith>
-      <HorizontalRule />
-     
-      <ForgotPassword>Forgot Password ?</ForgotPassword>
-    </MainContainer>
-               
-            </body>
-        );
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const validateEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setEmailError('');
+    setPasswordError('');
+
+    if (!email) {
+      setEmailError('Email cannot be empty');
+      return;
     }
-}
 
-const MainContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  height: 80vh;
-  width: 30vw;
-  background: rgba(255, 255, 255, 0.15);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(8.5px);
-  -webkit-backdrop-filter: blur(8.5px);
-  border-radius: 10px;
-  color: #ffffff;
-  text-transform: uppercase;
-  letter-spacing: 0.4rem;
-  @media only screen and (max-width: 320px) {
-    width: 80vw;
-    height: 90vh;
-    hr {
-      margin-bottom: 0.3rem;
+    if (!validateEmail(email)) {
+      setEmailError('Invalid email format');
+      return;
     }
-    h4 {
-      font-size: small;
+
+    if (!password) {
+      setPasswordError('Password cannot be empty');
+      return;
     }
-  }
-  @media only screen and (min-width: 360px) {
-    width: 80vw;
-    height: 90vh;
-    h4 {
-      font-size: small;
-    }
-  }
-  @media only screen and (min-width: 411px) {
-    width: 80vw;
-    height: 90vh;
-  }
 
-  @media only screen and (min-width: 768px) {
-    width: 80vw;
-    height: 80vh;
-  }
-  @media only screen and (min-width: 1024px) {
-    width: 70vw;
-    height: 50vh;
-  }
-  @media only screen and (min-width: 1280px) {
-    width: 30vw;
-    height: 80vh;
-  }
-`;
+    // If all validations pass, you can proceed with the sign-in logic
+    // For this example, let's just log the values
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
 
-const WelcomeText = styled.h2`
-  margin: 3rem 0 2rem 0;
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  height: 20%;
-  width: 100%;
-`;
-
-const ButtonContainer = styled.div`
-  margin: 1rem 0 2rem 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const LoginWith = styled.h5`
-  cursor: pointer;
-`;
-
-const HorizontalRule = styled.hr`
-  width: 90%;
-  height: 0.3rem;
-  border-radius: 0.8rem;
-  border: none;
-  background: linear-gradient(to right, #14163c 0%, #03217b 79%);
-  background-color: #ebd0d0;
-  margin: 1.5rem 0 1rem 0;
-  backdrop-filter: blur(25px);
-`;
-
-const IconsContainer = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  margin: 2rem 0 3rem 0;
-  width: 80%;
-`;
-
-const ForgotPassword = styled.h4`
-  cursor: pointer;
-`;
+  return (
+    <div className="div1">
+    <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email or mobile phone number</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          {emailError && <div className="validation-feedback invalid">{emailError}</div>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {passwordError && <div className="validation-feedback invalid">{passwordError}</div>}
+        </div>
+        <div className="form-group">
+          <button type="submit" className="signin-button w-100 btncolor">Sign-In</button>
+        </div>
+      </form>
+      <div className="help-section">
+        <a href="#">Forgot your password?</a>
+        <hr />
+        <p>New to 
+          ThriftTrade?</p>
+        <a href="#">Create your ThriftTrade account</a>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
