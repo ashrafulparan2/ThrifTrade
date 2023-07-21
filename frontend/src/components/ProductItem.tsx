3ae357482211505518877ceedddf073f1688870f
ashrafulparan2 <ashrafulparan@gmail.com>
2023-07-22 03:11:06 +0600

    adjusted height and added products

--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -1,50 +1,52 @@
-import { Button, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Product } from "../types/Product.js";
-import Rating from "./Rating.js";
-import { convertProductToCartItem } from "../utils.js";
-import { Store } from "../Store.js";
-import { useContext } from "react";
-import { CartItem } from "../types/Cart.js";
-import { toast } from "react-toastify";
-
-
-
-
+import { useContext } from 'react'
+import { Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { Store } from '../Store.js'
+import { CartItem } from '../types/Cart.js'
+import { Product } from '../types/Product.js'
+import { convertProductToCartItem } from '../utils.js'
+import Rating from './Rating.js'
 
 function ProductItem({ product }: { product: Product }) {
-  const { state, dispatch } = useContext(Store);
+  const { state, dispatch } = useContext(Store)
   const {
     cart: { cartItems },
-  } = state;
+  } = state
 
   const addToCartHandler = (item: CartItem) => {
-    const existItem = cartItems.find((x) => x._id === product._id);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const existItem = cartItems.find((x) => x._id === product._id)
+    const quantity = existItem ? existItem.quantity + 1 : 1
     // console.log();
     if (product.countInStock < quantity) {
-      alert("Sorry. Product is out of stock");
-      return;
+      alert('Sorry. Product is out of stock')
+      return
     }
     // console.log(quantity);
     //  dispatch({
     //   type:"CART_ADD_ITEM",
     //   payload:{...item}
     //  })
-    console.log(state);
+    console.log(state)
     dispatch({
-      type: "CART_ADD_ITEM",
+      type: 'CART_ADD_ITEM',
       payload: { ...item, quantity },
-    });
-    toast.success("Product added to the cart",{
+    })
+    toast.success('Product added to the cart', {
       autoClose: 1000,
     })
-  };
+  }
 
   return (
-    <Card>
+    <Card className="card-height-adjustment">
       <Link to={`/product/${product.slug}`}>
-        <img src={product.image} className="card-img-top" alt={product.name} />
+        <div className="image-height-adjustment">
+          <img
+            src={product.image}
+            className="card-img-top"
+            alt={product.name}
+          />
+        </div>
       </Link>
       <Card.Body>
         <Link className="product_name_class" to={`/product/${product.slug}`}>
@@ -65,7 +67,7 @@ function ProductItem({ product }: { product: Product }) {
         )}
       </Card.Body>
     </Card>
-  );
+  )
 }
 
-export default ProductItem;
+export default ProductItem
