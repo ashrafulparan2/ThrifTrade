bfe07e92161eaf47e3df69bce9249451837b5a99
ashrafulparan2 <ashrafulparan@gmail.com>
2023-07-29 15:07:04 +0600

    goldcard page improved

--- frontend/src/pages/MembershipPage.tsx
+++ frontend/src/pages/MembershipPage.tsx
@@ -1,29 +1,29 @@
-import { Badge, Container } from "react-bootstrap";
+import { Badge, Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function MembershipPage(){
     return(
         <div>
-            <Container>
+            <div>
             {/* <img src="../../images/membership.png" className="membership-image"></img> */}
             <div className="main-banner position-relative ">
               <img
-                src="../../images/membership.png"
+                src="../../images/goldcard7.png"
                 className="img-fluid rounded-3 membership-image"
                 alt="main banner"
               />
               <div className="main-banner-content position-absolute">
                 <Link to={'/membershiporder/'}>
                   <h3>
-                <Badge bg="secondary">Buy Now</Badge>
+                <Button className="offer-button">Buy Now</Button >
                 </h3>
                 </Link>
               </div>
               </div>
             
-            <img src="../../images/gold.png" className="membership-image"></img>
+            {/* <img src="../../images/gold.png" className="membership-image"></img> */}
             
-            </Container>
+            </div>
         </div>
     )
 }
